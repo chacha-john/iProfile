474744f91059a19e955cb8d10dd59108c5f15c2b
chacha <chachaerickjo@gmail.com>
2022-09-02 11:37:58 +0300

    ft - add logic for creating accounts and logging in

--- app/src/main/java/com/chachaup/iprofile/ui/CreateAccountActivity.java
+++ app/src/main/java/com/chachaup/iprofile/ui/CreateAccountActivity.java
@@ -1,5 +1,6 @@
 package com.chachaup.iprofile.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,9 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chachaup.iprofile.R;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +29,9 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     @BindView(R.id.textViewLoginLink)
     TextView mLoginLink;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,12 +40,15 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
 
         mCreateAccountButton.setOnClickListener(this);
         mLoginLink.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        CreateAuthStateListener();
     }
 
     @Override
     public void onClick(View v) {
         if (v == mCreateAccountButton){
-
+            CreateNewUser();
         }
         if (v == mLoginLink){
             Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
@@ -46,5 +56,52 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
             startActivity(intent);
         }
 
+    }
+
+    private void CreateNewUser(){
+        final String name = mUsernameCreateAccount.getEditText().getText().toString().trim();
+        final String email = mEmailCreateAccount.getEditText().getText().toString().trim();
+        final String password = mPasswordCreateAccount.getEditText().getText().toString().trim();
+        final String repeatPassword = mRepeatPasswordCreateAccount.getEditText().getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()){
+                        Toast.makeText(getApplicationContext(),"Account created successfully",Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+
+                    }
+                });
+    }
+
+    private void CreateAuthStateListener(){
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    Intent intent = new Intent(CreateAccountActivity.this, RandomProfileActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
--- app/src/main/java/com/chachaup/iprofile/ui/RandomProfileActivity.java
+++ app/src/main/java/com/chachaup/iprofile/ui/RandomProfileActivity.java
@@ -1,8 +1,14 @@
 package com.chachaup.iprofile.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,6 +19,7 @@ import com.chachaup.iprofile.models.IProfileResponse;
 import com.chachaup.iprofile.models.Result;
 import com.chachaup.iprofile.network.RandomUserAPI;
 import com.chachaup.iprofile.network.RandomUserClient;
+import com.google.firebase.auth.FirebaseAuth;
 import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
@@ -88,5 +95,30 @@ public class RandomProfileActivity extends AppCompatActivity implements View.OnC
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_overflow, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout){
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(RandomProfileActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
