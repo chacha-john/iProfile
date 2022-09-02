692ee4c3c736eb93bd19835258eb3561976153f7
chacha <chachaerickjo@gmail.com>
2022-09-02 08:42:32 +0300

    ft - add login activity ui

---
+++ app/src/main/java/com/chachaup/iprofile/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.chachaup.iprofile.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chachaup.iprofile.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
