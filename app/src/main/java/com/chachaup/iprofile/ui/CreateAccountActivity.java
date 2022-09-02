a3de0c5d3a9528defaf564f48db7cd7339acbdad
chacha <chachaerickjo@gmail.com>
2022-09-02 09:36:41 +0300

    ft - add ui for signup

---
+++ app/src/main/java/com/chachaup/iprofile/ui/CreateAccountActivity.java
@@ -0,0 +1,16 @@
+package com.chachaup.iprofile.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chachaup.iprofile.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
